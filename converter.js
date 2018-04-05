47f330c945b0f2f5e16da99d2630d37267d9b13f
awiles808 <awiles808@gmail.com>
2018-04-04 22:04:11 -0600

    Converting and Added CSS File

--- converter.js
+++ converter.js
@@ -1,15 +1,58 @@
-var converter_endpoint = "http://data.fixer.io/api/latest?access_key=9b8defd0b79b33255bd43bf46d261e97"
 
-var curCon;
-    $(function(){
+let countryName;                                        //Declaring The Variable "Counrty Name"
 
-    })
+$(".currencyBtn").on("click", function() {                 //Creating eventListner
+
+   countryName = $(this).val();
+
+   $.get("http://data.fixer.io/api/latest?access_key=9b8defd0b79b33255bd43bf46d261e97", function(response){
+       console.log(response);
 
 
-function convertCurrency(event) {
-  if ('type' in event.target && event.target.type === 'button') {
-    console.log('handle currency conversion here');
-  }
-}
+       response.rates[countryName]                   //Use Get The Exchange Rate For The Specific Conuntry
 
-document.body.addEventListener('click', convertCurrency);
+                                                    //Input Starts In Euros, Need To Convert To Dollars, Then The Need Country's Currency
+
+                                                    //this is the amount of dollars typed in
+       let dollarInput = $("input[type=text]").val();
+       let euroEquivalent = dollarInput / response.rates["USD"];
+       let finalAnswer = euroEquivalent * response.rates[countryName];
+
+                                                    // append the final anwser.
+       $("#output_amount").append("<p>  Total equals " + finalAnswer  + " " + countryName + ". </p>")
+
+   })
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
