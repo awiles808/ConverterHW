c02380c76827c0365b7492fce17759bb3ce215c1
awiles808 <awiles808@gmail.com>
2018-04-04 23:38:14 -0600

    A little Style with Comments

--- converter.js
+++ converter.js
@@ -1,25 +1,24 @@
 
-let countryName;                                        //Declaring The Variable "Counrty Name"
+let countryName;                                                     //Declaring The Variable "Counrty Name"
 
-$(".currencyBtn").on("click", function() {                 //Creating eventListner
+$(".currencyBtn").on("click", function() {                          //Creating eventListner, And Function()
 
-   countryName = $(this).val();
+   countryName = $(this).val();                                     //Using jQuery to call For The eventListner And Get The Value
 
-   $.get("http://data.fixer.io/api/latest?access_key=9b8defd0b79b33255bd43bf46d261e97", function(response){
-       console.log(response);
+   $.get("http://data.fixer.io/api/latest?access_key=9b8defd0b79b33255bd43bf46d261e97", function(response){     //Retrieving Data From API URl Endpoint
+       console.log(response);                                                                                   //Console Log Response From The Function.
 
 
-       response.rates[countryName]                   //Use Get The Exchange Rate For The Specific Conuntry
+       response.rates[countryName]                                  //Geting The Exchange Rate For The Specific Conuntry
 
-                                                    //Input Starts In Euros, Need To Convert To Dollars, Then The Need Country's Currency
 
-                                                    //this is the amount of dollars typed in
-       let dollarInput = $("input[type=text]").val();
-       let euroEquivalent = dollarInput / response.rates["USD"];
+
+       let dollarInput = $("input[type=text]").val();               //Input Starts In Euros, Need To Convert To Dollars, Then The Need Country's Currency
+       let euroEquivalent = dollarInput / response.rates["USD"];    //This Is The Amount Of Dollars Typed In.
        let finalAnswer = euroEquivalent * response.rates[countryName];
 
-                                                    // append the final anwser.
-       $("#output_amount").append("<p>  Total equals " + finalAnswer  + " " + countryName + ". </p>")
+
+       $("#output_amount").append("<li>  Total equals " + finalAnswer  + " " + countryName + ". </li>")// Append The Final Anwser.
 
    })
 });
